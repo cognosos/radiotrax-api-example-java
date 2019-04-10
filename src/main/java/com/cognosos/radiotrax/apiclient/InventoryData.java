53cb217e48d8578eb5931e24b5390c3d4e898664
Joe Koberg <joe.koberg@cognosos.com>
2019-04-10 13:36:25 -0400

    Correct Typo

--- src/main/java/com/cognosos/radiotrax/apiclient/InventoryData.java
+++ src/main/java/com/cognosos/radiotrax/apiclient/InventoryData.java
@@ -15,7 +15,7 @@ public class InventoryData {
         inventoryMap.put("asset_identifier", "A1001");
         inventoryMap.put("VIN", "1FTFW1RG0HFA66295");
         inventoryMap.put("Make", "Ford");
-        inventoryMap.put("Model", "150");
+        inventoryMap.put("Model", "F-150");
         inventoryMap.put("Style", "Raptor (3.5 V6) CREW CAB PICKUP 4-DR");
         return inventoryMap;
     }
